39c5089768b4645af3058576ef59c3bf0197932c
Walizada300 <walizada.300@gmail.com>
2025-06-29 12:11:22 +0430

    Initial Commit

---
+++ FitScale/FitScaleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitScaleApp.swift
+//  FitScale
+//
+//  Created by RTA Media on 6/29/25.
+//
+
+import SwiftUI
+
+@main
+struct FitScaleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FitScaleTests/FitScaleTests.swift
@@ -0,0 +1,17 @@
+//
+//  FitScaleTests.swift
+//  FitScaleTests
+//
+//  Created by RTA Media on 6/29/25.
+//
+
+import Testing
+@testable import FitScale
+
+struct FitScaleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
